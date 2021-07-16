64e9470fa6bcb34cf6b5caa07056672127d37e3b
Marcelo Dias <teiao.dias@gmail.com>
2021-07-16 10:39:08 -0300

    Adicionar arquivos de projeto.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var x = Request.Url.Query;
+
+            if (Request.Files.Count >= 1)
+            {
+                HttpPostedFileBase f = Request.Files[0];
+            }
+            
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page exibit.";
+
+            return View();
+        } 
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
